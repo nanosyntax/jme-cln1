b0e18187779044c467a1ec2a14a7f6bd96274877
mist <mist@auf.net>
2020-04-19 22:32:33 +0200

    test copy of jme with one change, new .git w/ origin + upstream

---
+++ app/features/config/index.js
@@ -0,0 +1,37 @@
+
+export default {
+    /**
+     * The URL with extra information about the app / service.
+     */
+    aboutURL: 'https://jitsi.org/what-is-jitsi/',
+
+    /**
+     * The URL to the source code repository.
+     */
+    sourceURL: 'https://github.com/jitsi/jitsi-meet-electron',
+
+    /**
+     * Application name.
+     */
+    appName: 'Jitsi Meet',
+
+    /**
+     * The default server URL of Jitsi Meet Deployment that will be used.
+     */
+    defaultServerURL: 'https://meet.jit.si',
+
+    /**
+     * URL to send feedback.
+     */
+    feedbackURL: 'mailto:support@jitsi.org',
+
+    /**
+     * The URL of Privacy Policy Page.
+     */
+    privacyPolicyURL: 'https://jitsi.org/meet/privacy',
+
+    /**
+     * The URL of Terms and Conditions Page.
+     */
+    termsAndConditionsURL: 'https://jitsi.org/meet/terms'
+};
